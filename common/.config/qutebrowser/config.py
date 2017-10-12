f64b9d0793820241eebb4e388d38d2338feafcd3
Hellux <hellux07@gmail.com>
2017-10-12 18:29:22 +0200

    added cfgs for new qutebrowser

---
+++ common/.config/qutebrowser/config.py
@@ -0,0 +1,12 @@
+c.fonts.hints = 'bold 7pt monospace'
+c.fonts.monospace = 'Source Code Pro'
+c.fonts.web.size.default = 18
+c.fonts.web.size.default_fixed = 18
+c.fonts.web.size.minimum = 18
+c.fonts.web.size.minimum_logical = 18
+
+c.downloads.remove_finished = 0
+
+config.bind('e', 'hint')
+config.bind('q', 'spawn mpv {url}');
+config.bind('Q', 'spawn mpv {hint-url}');
