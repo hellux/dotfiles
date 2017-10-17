b107bbac08d7b380ed2b2d1d95b4222692d21b56
Hellux <hellux07@gmail.com>
2017-10-17 12:57:43 +0200

    resized hints

--- common/.config/qutebrowser/config.py
+++ common/.config/qutebrowser/config.py
@@ -1,25 +1,26 @@
 ui_size = 12
-content_size = 18
 c.fonts.monospace = 'Source Code Pro'
-c.fonts.completion.entry = "%dpt monospace" % ui_size
-c.fonts.completion.category = "bold %dpt monospace" % ui_size
-c.fonts.debug_console = "%dpt monospace" % ui_size
-c.fonts.downloads = "%dpt monospace" % ui_size
-c.fonts.hints = "bold %dpt monospace" % ui_size
-c.fonts.keyhint = "%dpt monospace" % ui_size
-c.fonts.messages.error = "%dpt monospace" % ui_size
-c.fonts.messages.info = "%dpt monospace" % ui_size
-c.fonts.messages.warning = "%dpt monospace" % ui_size
-c.fonts.prompts = "%dpt sans-serif" % ui_size
-c.fonts.statusbar = "%dpt monospace" % ui_size
-c.fonts.tabs = "%dpt monospace" % ui_size
+c.fonts.completion.entry = '%dpt monospace' % ui_size
+c.fonts.completion.category = 'bold %dpt monospace' % ui_size
+c.fonts.debug_console = '%dpt monospace' % ui_size
+c.fonts.downloads = '%dpt monospace' % ui_size
+c.fonts.hints = 'bold %dpt monospace' % (ui_size-1)
+c.fonts.keyhint = '%dpt monospace' % ui_size
+c.fonts.messages.error = '%dpt monospace' % ui_size
+c.fonts.messages.info = '%dpt monospace' % ui_size
+c.fonts.messages.warning = '%dpt monospace' % ui_size
+c.fonts.prompts = '%dpt sans-serif' % ui_size
+c.fonts.statusbar = '%dpt monospace' % ui_size
+c.fonts.tabs = '%dpt monospace' % ui_size
+
+content_size = 18
 c.fonts.web.size.default = content_size
 c.fonts.web.size.default_fixed = content_size
 c.fonts.web.size.minimum = content_size
 c.fonts.web.size.minimum_logical = content_size
 
 c.content.geolocation = False
-c.content.headers.accept_language = "ru-RU,ru"
+c.content.headers.accept_language = 'ru-RU,ru'
 
 c.downloads.remove_finished = 0
 
