32e8413cfa84c381323f8aec821741a0632adf67
Hellux <hellux07@gmail.com>
2017-10-17 09:13:04 +0200

    lowered dpi, increased font sizes

--- common/.config/qutebrowser/config.py
+++ common/.config/qutebrowser/config.py
@@ -1,5 +1,16 @@
-c.fonts.hints = 'bold 7pt monospace'
 c.fonts.monospace = 'Source Code Pro'
+c.fonts.completion.entry = "11pt monospace"
+c.fonts.completion.category = "bold 11pt monospace"
+c.fonts.debug_console = "11pt monospace"
+c.fonts.downloads = "11pt monospace"
+c.fonts.hints = "bold 10pt monospace"
+c.fonts.keyhint = "11pt monospace"
+c.fonts.messages.error = "11pt monospace"
+c.fonts.messages.info = "11pt monospace"
+c.fonts.messages.warning = "11pt monospace"
+c.fonts.prompts = "11pt sans-serif"
+c.fonts.statusbar = "11pt monospace"
+c.fonts.tabs = "11pt monospace"
 c.fonts.web.size.default = 18
 c.fonts.web.size.default_fixed = 18
 c.fonts.web.size.minimum = 18
@@ -12,5 +23,5 @@ c.downloads.remove_finished = 0
 
 config.bind('e', 'hint')
 config.bind('E', 'hint all tab')
-config.bind('q', 'spawn mpv {url}');
-config.bind('Q', 'hint links spawn mpv {hint-url}');
+config.bind('q', 'spawn ~/scripts/stream.sh {url}');
+config.bind('Q', 'hint links spawn ~/scripts/stream.sh {hint-url}');
