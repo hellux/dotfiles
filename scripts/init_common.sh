6c7c425fc6208b04e42e68fe82330f1a8825c144
Hellux <hellux07@gmail.com>
2017-11-03 09:29:58 +0100

    fixed sway kbd layout issues

--- scripts/init_common.sh
+++ scripts/init_common.sh
@@ -1,6 +1,3 @@
-export XKB_DEFAULT_LAYOUT=se
-export XKB_DEFAULT_VARIANT=dvorak
-export XKB_DEFAULT_OPTIONS=caps:escape
-setxkbmap se -variant dvorak -option caps:escape
-
 [[ -f ~/.Xresources ]] && xrdb -merge -I$HOME ~/.Xresources
+
+setxkbmap se -variant dvorak -option caps:escape
