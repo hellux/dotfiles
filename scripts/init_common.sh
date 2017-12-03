58da986945cb2ae7cb597c1de97f49bf36327a42
Hellux <hellux07@gmail.com>
2017-12-03 20:39:54 +0100

    added quickmark, silenced rm error

--- scripts/init_common.sh
+++ scripts/init_common.sh
@@ -3,7 +3,7 @@
 setxkbmap se -variant dvorak -option caps:escape
 
 # remove files older than a day in Downloads
-find ~/Downloads -mtime +1 -exec rm -rf {} \;
+find ~/Downloads -mtime +1 -exec rm -rf {} 2> /dev/null \
 
 # merge common and local i3 config
 cat ~/.config/i3/config.common \
