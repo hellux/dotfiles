6f16354218df812065cb9fcdaad7c6048bb4019b
Hellux <hellux07@gmail.com>
2017-12-16 22:51:47 +0100

    changed xdg basedirs

--- scripts/init_common.sh
+++ scripts/init_common.sh
@@ -3,9 +3,6 @@
 
 setxkbmap se -variant dvorak -option caps:escape
 
-# remove files older than a day in Downloads
-find ~/Downloads -mtime +1 -exec rm -rf {} 2> /dev/null \
-
 # merge common and local i3 config
 cat ~/.config/i3/config.common \
     ~/.config/i3/config.local > ~/.config/i3/config
