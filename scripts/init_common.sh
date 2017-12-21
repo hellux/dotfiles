1841ead4d4e9e8f437e9dd66230580c0ef013f9a
Hellux <hellux07@gmail.com>
2017-12-21 11:38:48 +0100

    update vimb, create xdg dirs

--- scripts/init_common.sh
+++ scripts/init_common.sh
@@ -3,6 +3,8 @@
 
 setxkbmap se -variant dvorak -option caps:escape
 
+mkdir /tmp/xdg /tmp/download
+
 # merge common and local i3 config
 cat ~/.config/i3/config.common \
     ~/.config/i3/config.local > ~/.config/i3/config
