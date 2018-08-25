3d7ffe30cab321e8c6df109fffbfd1e6dc6b594a
hellux <hellux@protonmail.com>
2018-08-25 14:29:48 +0200

    update video script, rm qb quickmarks

--- scripts/play-video.sh
+++ scripts/play-video.sh
@@ -1,7 +1,7 @@
 #!/bin/env sh
-fifo=$HOME/.mp_pipe
+fifo=$XDG_RUNTIME_DIR/mp_pipe
 [ ! -e $fifo ] && mkfifo $fifo
-if ps -ao comm | grep -q mpv;
+if ps -eo comm | grep -q '^mpv$';
 then echo loadfile "$@" append-play > $fifo
-else mpv --input-file=$fifo "$@" &
+else mpv --ytdl-format="bestvideo+bestaudio" --input-file=$fifo "$@" &
 fi
