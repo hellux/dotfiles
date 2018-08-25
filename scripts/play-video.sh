27f829d8ade504d108d0bde99813b442029a8fd5
Noah Hellman <noah.hellman@protonmail.com>
2018-08-25 17:13:12 +0200

    correct typo, show stderr

--- scripts/play-video.sh
+++ scripts/play-video.sh
@@ -5,6 +5,6 @@ fifo=$XDG_RUNTIME_DIR/mp_pipe
 if ps -eo comm | grep -q '^mpv$'; then
     echo loadfile "$@" append-play > $fifo
 else
-    mpv --ytdl-format="bestvideo+bestaudio "--input-file=$fifo "$@" \
-        1> /dev/null 2> /dev/null &
+    mpv --ytdl-format="bestvideo+bestaudio" --input-file=$fifo "$@" \
+        2> /dev/null &
 fi
