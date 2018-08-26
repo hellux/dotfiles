89c46967ae0bbe0ad7968fe0a53f123ee6a53cd5
hellux <hellux@protonmail.com>
2018-08-26 12:27:53 +0200

    allow urls without split a/v

--- scripts/play-video.sh
+++ scripts/play-video.sh
@@ -2,9 +2,13 @@
 fifo=$XDG_RUNTIME_DIR/mp_pipe
 [ ! -e $fifo ] && mkfifo $fifo
 
+if youtube-dl -q -s -f "bestvideo+bestaudio" $@;
+then format="bestvideo+bestaudio"
+else format="best"
+fi
+
 if ps -eo comm | grep -q '^mpv$'; then
-    echo loadfile "$@" append-play > $fifo
+    echo loadfile "$@" append-play ytdl-format=$format > $fifo
 else
-    mpv --ytdl-format="bestvideo+bestaudio" --input-file=$fifo "$@" \
-        2> /dev/null &
+    mpv --ytdl-format=$format --really-quiet --input-file=$fifo $@
 fi
