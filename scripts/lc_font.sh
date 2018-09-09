4e48fc44868667870476a899581be692b27b4892
hellux <hellux@protonmail.com>
2018-09-09 20:35:25 +0200

    add another possible dir

--- scripts/lc_font.sh
+++ scripts/lc_font.sh
@@ -1,1 +1,11 @@
-setfont /usr/share/consolefonts/latarcyrheb-sun32.psfu.gz
+font="latarcyrheb-sun32.psfu.gz"
+dir1="/usr/share/consolefonts"
+dir2="/usr/share/kbd/consolefonts"
+
+if [ -r "$dir1/$font" ]; then
+    setfont "$dir1/$font"
+elif [ -r "$dir2/$font" ]; then
+    setfont "$dir2/$font"
+else
+    exit 1
+fi
