d9b1a08cf3d4ad99e8758bbd0ec1ed647f566b8e
Noah Hellman <noah.hellman@protonmail.com>
2020-10-15 13:52:45 +0200

    update configs

--- scripts/monitor_desktop.sh
+++ scripts/monitor_desktop.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 xrandr --auto
 xrandr --output DVI-D-0 --mode 1920x1080 --rate 120 --primary
-xrandr --output HDMI-A-1 --mode 1280x1024 --rate 75 --right-of DVI-D-0
-xrandr --output HDMI-A-0 --mode 1920x1080 --rate 60 --right-of HDMI-A-1
+xrandr --output HDMI-A-1 --mode 1920x1080 --rate 60 --left-of DVI-D-0
+xrandr --output HDMI-A-0 --mode 1920x1080 --rate 60 --right-of DVI-D-0
