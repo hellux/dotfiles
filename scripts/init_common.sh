0aa320d7f456d2066693939088fa550dd3f96506
Noah Hellman <noah.hellman@protonmail.com>
2020-11-21 09:59:38 +0100

    i3: merge common and local config

--- scripts/init_common.sh
+++ scripts/init_common.sh
@@ -7,7 +7,3 @@ fi
 mkdir -p /tmp/xdg /tmp/download
 
 setxkbmap se -variant dvorak -option caps:escape
-
-# merge common and local i3 config
-cat ~/.config/i3/config.common \
-    ~/.config/i3/config.local > ~/.config/i3/config
