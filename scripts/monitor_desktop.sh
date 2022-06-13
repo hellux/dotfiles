3a61b25989067082aced230f0b6ad9e5a04f3dfe
Noah Hellman <noah.hellman@protonmail.com>
2022-06-13 17:49:44 +0200

    i3: add monitor

--- scripts/monitor_desktop.sh
+++ scripts/monitor_desktop.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+primary="DisplayPort-0"
 xrandr --auto
-xrandr --output DVI-D-0 --mode 1920x1080 --rate 120 --primary
-xrandr --output HDMI-A-1 --mode 1920x1080 --rate 60 --left-of DVI-D-0
-xrandr --output HDMI-A-0 --mode 1920x1080 --rate 60 --right-of DVI-D-0
+xrandr --output "$primary" --mode 3840x2160 --rate 144 --primary
+xrandr --output HDMI-A-1 --mode 1920x1080 --rate 60 --left-of "$primary"
+xrandr --output DVI-D-0 --mode 1920x1080 --rate 120 --right-of "$primary"
