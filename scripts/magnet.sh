eddaa55d425b3fcb0aa5280dbd5f547a242f50b2
Noah Hellman <noah@hllmn.net>
2023-01-08 10:52:43 +0100

    magnet.sh for starting magnet links

---
+++ scripts/magnet.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+link=$1
+watch=${2:-/net/narwhal/torrent/watch};
+h=$(echo "$link" | awk -F'&' 'BEGIN {RS="xt=urn:btih:"} NR==2 {print $1}')
+[ -n "$h" ] && echo "d10:magnet-uri${#link}:${link}e" > "$watch/meta-$h.torrent"
