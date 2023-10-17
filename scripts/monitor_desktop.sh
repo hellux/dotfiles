a89b44f8165618303a2606e699e459c76912aab8
Noah Hellman <noah@hllmn.net>
2023-10-17 20:48:03 +0200

    update desktop for X

--- scripts/monitor_desktop.sh
+++ scripts/monitor_desktop.sh
@@ -2,5 +2,5 @@
 primary="DisplayPort-0"
 xrandr --auto
 xrandr --output "$primary" --mode 3840x2160 --rate 144 --primary
-xrandr --output HDMI-A-1 --mode 1920x1080 --rate 60 --left-of "$primary"
-xrandr --output DVI-D-0 --mode 1920x1080 --rate 120 --right-of "$primary"
+xrandr --output HDMI-A-0 --mode 2560x2880 --rate 60 --left-of "$primary"
+xrandr --output DisplayPort-1 --mode 1920x1080 --rate 60 --right-of "$primary"
