d7288184c835dc5cd5b3bbd7fb02d38579becb8f
Noah Hellman <noah@hllmn.net>
2025-08-16 11:39:51 +0200

    rm unused scripts

--- scripts/5590/rebuild.sh
+++
@@ -1,8 +0,0 @@
-set -u
-cd /usr/src/linux || exit
-make -j8 all
-make modules_install
-mount /boot
-make install
-release=$(cat include/config/kernel.release)
-cp "/boot/vmlinuz-$release" "/boot/efi/boot/bootx64.efi"
--- scripts/monitor_desktop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-primary="DisplayPort-0"
-xrandr --auto
-xrandr --output "$primary" --mode 3840x2160 --rate 144 --primary
-xrandr --output HDMI-A-0 --mode 2560x2880 --rate 60 --left-of "$primary"
-xrandr --output DisplayPort-1 --mode 1920x1080 --rate 60 --right-of "$primary"
